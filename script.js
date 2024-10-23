0ca3e9f764aa73c7b9e89bc2fa8523687b35adb9
Ritank Jaikar <ritank.jaikar@gmail.com>
2024-10-23 14:56:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+const width = document.querySelector("#width");
+const height = document.querySelector("#height");
+
+function updateDimensions() {
+    width.innerText = window.innerWidth;
+    height.innerText = window.innerHeight;
+}
+
+updateDimensions();
+
+window.addEventListener("resize", updateDimensions);
+
+
+
